b834954a543227a1b1b457e380c8adf30b98c282
arybotz <95002729+arybotz@users.noreply.github.com>
2022-07-26 15:46:38 +0700

    Initial commit

---
+++ plugins/info-tutorialsession.js
@@ -0,0 +1,43 @@
+/*============== Jangan di ganti! ==============*/
+
+const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia } = require('@adiwajshing/baileys')
+let fs = require('fs')
+let handler = async (m) => {
+let krizyn = `
+*SUBSCRIBE BANH!!*
+	`.trim()
+  let message = await prepareWAMessageMedia({ image: await (await require('node-fetch')(fotonya8)).buffer()}, { upload: conn.waUploadToServer }) 
+    const template = generateWAMessageFromContent(m.key.remoteJid, proto.Message.fromObject({
+        templateMessage: {
+            hydratedTemplate: {
+                hydratedContentText: krizyn,
+                imageMessage: message.imageMessage, 
+           hydratedFooterText: '',
+           hydratedButtons: [{
+             urlButton: {
+               displayText: 'Subscribe Yt😌',
+               url: youtobe
+             }
+
+           },
+                {
+               urlButton: {
+               displayText: 'Subscribe Yt😌',
+               url: youtobe
+             }
+           }]
+         }
+       }
+     }), { userJid: m.sender, quoted: m });
+    //conn.reply(m.chat, text.trim(), m)
+    return await conn.relayMessage(
+         m.chat,
+         template.message,
+         { messageId: template.key.id }
+     )
+}
+
+handler.tags = ['main', 'info']
+handler.command = /^(youtobe)$/i
+handler.help = ['youtobe']
+module.exports = handler
